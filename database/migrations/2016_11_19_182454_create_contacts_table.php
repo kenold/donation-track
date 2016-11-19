d1f3ba078e8b7f4ff3463a93e309ed25ce300931
Kenold Beauplan <bkenold@gmail.com>
2016-11-19 17:32:04 -0500

    update contacts migrations

--- database/migrations/2016_11_19_182454_create_contacts_table.php
+++ database/migrations/2016_11_19_182454_create_contacts_table.php
@@ -15,7 +15,26 @@ class CreateContactsTable extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('fname', 100);
+            $table->char('mname', 1);
+            $table->string('lname', 100);
+            $table->char('sex', 1);
+            $table->date('dob');
+            $table->string('job_title', 100);
+            $table->string('company', 100);
+            $table->string('address1', 200);
+            $table->string('address2', 200);
+            $table->string('city', 100);
+            $table->char('state', 2)->default('FL');
+            $table->mediumInteger('zip')->unsigned();
+            $table->string('country', 100);
+            $table->string('cell_phone', 20);
+            $table->string('work_phone', 20);
+            $table->string('email', 100);
+            $table->string('facebook', 150);
+            $table->string('twitter', 150);
+            $table->string('skype');
+            $table->text('notes');
             $table->timestamps();
         });
     }
