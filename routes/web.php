47fa20fd041b2904b11230c8b912eb717bf13f9e
Kenold Beauplan <bkenold@gmail.com>
2016-11-19 19:27:43 -0500

    test relationships

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,22 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('contact/{id}', function ($id) {
+    $contact = App\Contact::find($id);
+    echo $contact->fname . ", " . $contact->lname;
+
+    echo '<ul>';
+    foreach ($contact->donations as $donation) {
+        echo '<li>' . $donation->item_name . '</li>';
+    }
+    echo '</ul>';
+});
+
+Route::get('/donations', function() {
+    $donations = App\Donation::all();
+
+    foreach($donations as $donation) {
+        echo $donation->contact->fname . ' donated ' . $donation->item_name . ' on ' . $donation->created_at . '<br>';
+    }
+});
