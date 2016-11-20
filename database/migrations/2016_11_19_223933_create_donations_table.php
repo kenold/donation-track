47ee2a992493d93b008bf1e86bc45f1c3d4e7b2a
Kenold Beauplan <bkenold@gmail.com>
2016-11-19 19:27:19 -0500

    add foreign keys

---
+++ database/migrations/2016_11_19_223933_create_donations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDonationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('donations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('contact_id')->unsigned();
+            //$table->dateTime('date');
+            $table->string('item_name', 255);
+            $table->bigInteger('quantity');
+            $table->foreign('contact_id')->references('id')->on('contacts');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('donations');
+    }
+}
