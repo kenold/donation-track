108b5c61032964deba11a8daabda1e68634f8d6b
Kenold Beauplan <bkenold@gmail.com>
2016-11-21 13:36:55 -0500

    update category seeder

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -11,6 +11,8 @@ class CategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('categories')->insert([
+            'name' => 'Medicine',
+        ]);
     }
 }
