5019d4ad1d6925ce25186d54bb75c2aa755f40fb
Kenold Beauplan <bkenold@gmail.com>
2016-11-21 12:09:40 -0500

    add contact photo url

--- database/migrations/2016_11_19_182454_create_contacts_table.php
+++ database/migrations/2016_11_19_182454_create_contacts_table.php
@@ -17,24 +17,24 @@ class CreateContactsTable extends Migration
             $table->increments('id');
             $table->string('fname', 250);
             $table->string('lname', 250);
-            /*$table->char('mname', 1)->nullable();
+            $table->char('mname', 1)->nullable();
             $table->char('sex', 1);
-            $table->date('dob');
+            $table->string('photo_url', 250);
             $table->string('job_title', 100);
             $table->string('company', 100);
             $table->string('address1', 250);
             $table->string('address2', 200);
             $table->string('city', 250);
             $table->char('state', 2)->default('FL');
-            $table->mediumInteger('zip')->unsigned();
+            $table->string('zip', 10);
             $table->string('country', 100);
-            $table->string('cell_phone', 20);
-            $table->string('work_phone', 20);
+            $table->string('cell_phone', 50);
+            $table->string('work_phone', 50);
             $table->string('email', 100);
             $table->string('facebook', 150);
             $table->string('twitter', 150);
             $table->string('skype');
-            $table->text('notes');*/
+            $table->text('notes');
             $table->timestamps();
         });
     }
