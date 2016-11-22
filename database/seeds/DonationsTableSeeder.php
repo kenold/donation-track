a4732189575d925299638f57c786bed6a5de1f4c
Kenold Beauplan <bkenold@gmail.com>
2016-11-22 13:14:11 -0500

    set item_id default value

--- database/seeds/DonationsTableSeeder.php
+++ database/seeds/DonationsTableSeeder.php
@@ -23,7 +23,7 @@ class DonationsTableSeeder extends Seeder
                 'date' => $faker->date($format = 'Y-m-d', $max = 'now'),
                 'contact_id' => rand(1, 20),
                 'amount' => $faker->randomFloat($nbMaxDecimals = NULL, $min = 10, $max = 800),
-                'item_id' => rand(1, 5),
+                'item_id' => 1, //rand(1, 5),
                 'quantity' => $faker->numberBetween($min = 5, $max = 100),
                 'created_at' => date("Y-m-d H:i:s"),
                 'updated_at' => date("Y-m-d H:i:s")
