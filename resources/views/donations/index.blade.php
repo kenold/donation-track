7bb287bba609ceaf181ce13bd0cfe2ef128a9e11
Kenold Beauplan <bkenold@gmail.com>
2016-11-22 17:19:23 -0500

    display contact last name

--- resources/views/donations/index.blade.php
+++ resources/views/donations/index.blade.php
@@ -1,7 +1,12 @@
+@extends('layouts.main')
+
+@section('content')
 <ul>
     @foreach ($donations as $donation)
-        <h1>{{ $donation->contact_id }}</h1>
+        <h1>{{ $donation->contact->fname }}, {{ $donation->contact->lname }}</h1>
         <li>Amount: ${{ $donation->amount }}</li>
         <li>Item: {{ $donation->item_id }} ({{ $donation->item_qty}})</li>
+        <li>Date: {{ $donation->created_at->diffInYears(Carbon\Carbon::now()) }}</li>
     @endforeach
 </ul>
+@endsection
