40e088094b9f8866b09d2bee9ab7937903030580
Kenold Beauplan <bkenold@gmail.com>
2016-11-22 13:15:26 -0500

    remove routes testing

--- routes/web.php
+++ routes/web.php
@@ -14,26 +14,3 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('contact/{id}', function ($id) {
-    $contact = App\Contact::find($id);
-    echo "<img src= " . $contact->photo_url . " width=140 />";
-    echo "<h1>" . $contact->fname . ", " . $contact->lname . "</h1>";
-
-    echo '<ul>';
-
-    foreach ($contact->donations as $donation) {
-        $item = App\Item::find($donation->item_id);
-        echo '<li>' . $item->name . ' (' . $donation->quantity . ')' . '</li>';
-    }
-    echo '</ul>';
-});
-
-Route::get('/donations', function() {
-    $donations = App\Donation::all();
-
-    foreach($donations as $donation) {
-        $item = App\Item::find($donation->item_id);
-        echo $donation->contact->fname . ' donated ' . $item->name . ' on ' . $donation->created_at . '<br>';
-    }
-});
