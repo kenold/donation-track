bc1ad934a9c67155d653833897e999c2c926b9c1
Kenold Beauplan <bkenold@gmail.com>
2016-11-25 18:08:03 -0500

    add if statement to check if donations count

--- resources/views/donations/index.blade.php
+++ resources/views/donations/index.blade.php
@@ -1,23 +1,28 @@
 @extends('layouts.main')
 
 @section('content')
-
-    <table class="table">
-        <tr>
-            <th>ID</th>
-            <th>Date</th>
-            <th>Contact</th>
-            <th>Amount</th>
-            <th># of Items</th>
-        </tr>
-        @foreach ($donations as $donation)
+    @if(count($donations) > 0)
+        <table class="table">
             <tr>
-                <td>{{ $donation->id }}</td>
-                <td>{{ $donation->date }}</td>
-                <td>{{ $donation->contact->fname }}, {{ $donation->contact->lname }}</td>
-                <td>${{ $donation->amount }}</td>
-                <td>{{ $donation->item_qty}}</td>
+                <th>ID</th>
+                <th>Date</th>
+                <th>Contact</th>
+                <th>Amount</th>
+                <th># of Items</th>
             </tr>
-        @endforeach
-    </table>
+
+            @foreach ($donations as $donation)
+                <tr>
+                    <td>{{ $donation->id }}</td>
+                    <td>{{ $donation->date }}</td>
+                    <td>{{ $donation->contact->fname }}, {{ $donation->contact->lname }}</td>
+                    <td>${{ $donation->amount }}</td>
+                    <td>{{ $donation->item_qty}}</td>
+                </tr>
+            @endforeach
+        </table>
+    @else
+        <h2>No donations!</h2>
+    @endif
+
 @endsection
