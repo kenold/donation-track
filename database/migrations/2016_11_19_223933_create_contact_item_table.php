4c3324d2a444cf831815a4e0c6511956afc2385c
Kenold Beauplan <bkenold@gmail.com>
2016-12-02 16:41:12 -0500

    initial commit

---
+++ database/migrations/2016_11_19_223933_create_contact_item_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContactItemTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contact_item', function (Blueprint $table) {
+            $table->increments('id');
+            //$table->date('date')->default(date("Y-m-d"));
+            $table->integer('contact_id')->unsigned();
+            //$table->float('amount');
+            $table->integer('item_id')->unsigned();
+            //$table->bigInteger('item_qty');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //disable foreign key check for this connection before running seeders
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        Schema::dropIfExists('contact_item');
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+    }
+}
