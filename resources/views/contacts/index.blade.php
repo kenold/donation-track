936a7e57412947b6f438663b5928e78099913fff
Kenold Beauplan <bkenold@gmail.com>
2016-12-02 16:42:22 -0500

    add header

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -1,3 +1,9 @@
-@foreach ($contacts as $contact)
-    <p>{{ $contact->fname }} {{ $contact->mname }}. {{ $contact->lname }}</p>
-@endforeach
+@extends('layouts.main')
+
+@section('content')
+    <h1>Contacts</h1>
+    
+    @foreach ($contacts as $contact)
+        <p>{{ $contact->fname }} {{ $contact->mname }}. {{ $contact->lname }}</p>
+    @endforeach
+@endsection
