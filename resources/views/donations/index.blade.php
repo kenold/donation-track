1aa0000f0eb9ccfff7c16d47c11d49b61d9188b6
Kenold Beauplan <bkenold@gmail.com>
2016-12-05 14:37:17 -0500

    comment out most recent

--- resources/views/donations/index.blade.php
+++ resources/views/donations/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <h1>Donations</h1>
-    
+
     <table class="table">
         <tr>
             <th>Contact ID</th>
@@ -13,8 +13,14 @@
             <tr>
                 <td>{{ $contact->id }}</td>
                 <td>{{ $contact->fname }} {{ $contact->lname }}</td>
-                <td>{{ count($contact->items) }}</td>
+                <td>{{ count($contact->items) }} <br /></td>
             </tr>
         @endforeach
+
+        {{-- <h1>Most Recents</h1>
+        @foreach ($contacts->items as $item)
+            {{ $item->name }} <br />
+        @endforeach --}}
+
     </table>
 @endsection
