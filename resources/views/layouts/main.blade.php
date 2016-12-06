403ba827890e1a163a56f4f18a7e7625a6e979d5
Kenold Beauplan <bkenold@gmail.com>
2016-12-06 15:08:05 -0500

    update full-width layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,16 +4,15 @@
     @include('partials._nav')
 
     <div class="container">
-        <div class="row">
+        <div class="row">            
             <div class="col-md-12">
-                    @yield('content')
-
+                @yield('content')
             </div>
         </div>
     </div>
 
     <!-- Footer -->
     @include('partials._footer')
-    
+
 </body>
 </html>
