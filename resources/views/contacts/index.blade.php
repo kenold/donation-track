11ba161dfbe76bac80bcd2b0667c50d4ea7f0b10
Kenold Beauplan <bkenold@gmail.com>
2016-12-06 11:45:18 -0500

    display more fields in contact listing

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -2,8 +2,27 @@
 
 @section('content')
     <h1>Contacts</h1>
-    
-    @foreach ($contacts as $contact)
-        <p>{{ $contact->fname }} {{ $contact->mname }}. {{ $contact->lname }}</p>
-    @endforeach
+
+    <table class="table table-striped">
+        <tr>
+            <th>Name</th>
+            <th>Job Title</th>
+            <th>Company</th>
+            <th>Phone</th>
+            <th>Location</th>
+            <th>Email</th>
+        </tr>
+        @foreach ($contacts as $contact)
+            <tr>
+                <td>{{ $contact->fname }}
+                    {{ isset($contact->mname) ? $contact->mname.'.' : '' }}
+                    {{ $contact->lname }}</td>
+                <td>{{ $contact->job_title }}</td>
+                <td>{{ $contact->company }}</td>
+                <td>{{ $contact->cell_phone }}</td>
+                <td>{{ $contact->state }}</td>
+                <td>{{ $contact->email }}</td>
+            </tr>
+        @endforeach
+    </table>
 @endsection
