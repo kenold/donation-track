140c46235d2653ed0bb72ef31d6071297e78edde
Kenold Beauplan <bkenold@gmail.com>
2016-12-06 17:16:26 -0500

    eager load items

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -14,7 +14,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        $categories = Category::all();
+        $categories = Category::with('items')->get();
 
         return view('categories.index', compact('categories'));
     }
