0e43e0fac8342fa6aa437ce0cb174d1b77b30002
Kenold Beauplan <bkenold@gmail.com>
2016-12-06 13:17:00 -0500

    display categories sidebar

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -1,3 +1,29 @@
-@foreach ($items as $item)
-    <p>{{ $item->name }} - {{ $item->category_id }}</p>
-@endforeach
+@extends('layouts.main')
+
+@section('content')
+    <div class="col-md-3">
+    <div class="list-group">
+    <ul>
+        <li class="list-group-item">All <span class="badge">{{ $items->count() }}</span></li>
+        @foreach ($categories as $category)
+            <li class="list-group-item">{{ $category->name }} <span class="badge">{{ $category->items->count() }}</span></li>
+        @endforeach
+    </ul>
+    </div>
+</div>
+
+<div class="col-md-9">
+    <table class="table table-striped">
+        <tr>
+            <th>Name</th>
+            <th>Category</th>
+        </tr>
+        @foreach ($items as $item)
+            <tr>
+                <td>{{ $item->name }}</td>
+                <td>{{ $item->category->name }}</td>
+            </tr>
+        @endforeach
+    </table>
+</div>
+@endsection
