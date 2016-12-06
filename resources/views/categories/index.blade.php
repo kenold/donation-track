ee8f56449ae1eeedad9da6eb9d0bd2c5754fea42
Kenold Beauplan <bkenold@gmail.com>
2016-12-06 17:17:03 -0500

    add page header section

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,3 +1,17 @@
-@foreach ($categories as $category)
-    <p>{{ $category->name }}</p>
-@endforeach
+@extends('layouts.main')
+
+@section('page-header')
+    <h1>Categories Listing</h1>
+@endsection
+
+@section('content')
+
+        <div class="list-group">
+            @foreach ($categories as $category)
+                <a href="#" class="list-group-item">
+                <h4 class="list-group-item-heading">{{ $category->name }} <span class="badge">{{ $category->items->count() }}</span></h4>
+                <p class="list-group-item-text">{{ $category->notes }}</p>
+            </a>
+            @endforeach
+        </div>
+@endsection
--- resources/views/donations/index.blade.php
+++ resources/views/donations/index.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.main')
 
-@section('content')
-    @section('page-header')
-        <h1>Recent Donations</h1>
-    @endsection
+@section('page-header')
+    <h1>Recent Donations</h1>
+@endsection
 
-    <table class="table">
+@section('content')
+    <table class="table table-bordered table-responsive" id="filterTable">
         <thead>
             <tr>
                 <th>Contact ID</th>
@@ -13,15 +13,17 @@
                 <th># of Items Donated</th>
             </tr>
         </thead>
-        @foreach ($contacts as $contact)
-            <tr>
-                <td>{{ $contact->id }}</td>
-                <td><a href="{{ url('/donations/contact', $contact->id) }}">{{ $contact->fname }}
-                    {{ isset($contact->mname) ? $contact->mname.'.' : '' }}
-                    {{ $contact->lname }}</a></td>
-                <td>{{ count($contact->items) }} <br /></td>
-            </tr>
-        @endforeach
+        <tbody>
+            @foreach ($contacts as $contact)
+                <tr>
+                    <td>{{ $contact->id }}</td>
+                    <td><a href="{{ url('/donations/contact', $contact->id) }}">{{ $contact->fname }}
+                        {{ isset($contact->mname) ? $contact->mname.'.' : '' }}
+                        {{ $contact->lname }}</a></td>
+                    <td>{{ count($contact->items) }} <br /></td>
+                </tr>
+            @endforeach
+        </tbody>
 
         {{-- <h1>Most Recents</h1>
         @foreach ($contacts->items as $item)
